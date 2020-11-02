7628654257cb07036cdb59cb55eb4ebc2c0b2985
Laurie Cai <laurie801@gmail.com>
2020-11-01 17:37:27 -0800

    Add functions for add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,28 @@
+// ----- FUNCTIONS -----
+// Function: Add
+const add = function(a, b) {
+    console.log(a + b);
+}
+add(5, 2);
+add(42, 163);
+
+// Function: Subtract
+const subtract = function(a, b) {
+    console.log(a - b);
+}
+subtract(5, 1);
+subtract(1, 5);
+
+// Function: Multiply
+const multiply = function(a, b) {
+    console.log(a * b);
+}
+multiply(3, 3);
+multiply(4, 4);
+
+// Function: Divide
+const divide = function(a, b) {
+    console.log(a / b);
+}
+divide(4, 2);
+divide(3, 2);
