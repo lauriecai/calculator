477aa2e5ba1fa374cd2a23dd07b3752fe4e1a709
Laurie Cai <laurie801@gmail.com>
2020-11-01 19:54:04 -0800

    Add function for operate

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 // ----- FUNCTIONS -----
 // Function: Add
-const add = function(a, b) {
+function add(a, b) {
     console.log(a + b);
 }
 add(5, 2);
 add(42, 163);
 
 // Function: Subtract
-const subtract = function(a, b) {
+function subtract(a, b) {
     console.log(a - b);
 }
 subtract(5, 1);
 subtract(1, 5);
 
 // Function: Multiply
-const multiply = function(a, b) {
+function multiply(a, b) {
     console.log(a * b);
 }
 multiply(3, 3);
 multiply(4, 4);
 
 // Function: Divide
-const divide = function(a, b) {
+function divide(a, b) {
     console.log(a / b);
 }
 divide(4, 2);
 divide(3, 2);
+
+// Function: Operate
+function operate(a, b, operator) {
+    switch (operator) {
+        case 'add': 
+            add(a, b);
+            break;
+        case 'subtract': 
+            subtract(a, b);
+            break;
+        case 'multiply': 
+            multiply(a, b);
+            break;
+        case 'divide':
+            divide(a, b);
+            break;
+        default: console.log('huh?');
+    }
+}
+operate(7, 2, 'subtract');
+operate(7, 2, 'multiply');
